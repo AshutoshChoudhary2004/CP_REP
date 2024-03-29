3dc15d83bdbf2888c667113f9ac5122b15cd8231
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2024-03-30 01:52:17 +0530

    Update max-flow (dinics) .cpp

--- templates/graphs/flow/max-flow (dinics) .cpp
+++ templates/graphs/flow/max-flow (dinics) .cpp
@@ -1,6 +1,7 @@
 /*
 
 Returns the value of max flow
+No need of constructing graph, just init the value of total nodes(N) and add all the edges using add_edges
 Time Complexity : O(V * V * E)
 Space Complexity : O(V + E)
 
