50e9f5105c795ed7dbb4913eceff42260ea17c8a
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2024-04-01 00:57:20 +0530

    Update max-flow (dinics) .cpp

--- templates/graphs/flow/max-flow (dinics) .cpp
+++ templates/graphs/flow/max-flow (dinics) .cpp
@@ -1,196 +1,104 @@
-/*
+struct MaxFlow{
+    enum e_type{
+        directional,
+        directional_reverse,
+        bidirectional
+    };
+    struct Edge{
+        int v;
+        int rev;
+        ll cap;
+        ll ocap;
+        e_type type;
 
-Works for both directed, undirected
-Returns the value of max flow, edges that are in mincut along with their flow
-We can also print the flow on each edge after max flow
-To use : 
-
-dinic<ll> obj;
-obj.init(n)
-
-Time Complexity : O(V * V * E)
-Space Complexity : O(V + E)
-
-*/
-
-#include <algorithm>
-#include <array>
-#include <cassert>
-#include <iostream>
-#include <limits>
-#include <queue>
-#include <vector>
-using namespace std;
-
-const int INF = int(1e9) + 5;
-
-enum edge_type : uint8_t { DIRECTIONAL, DIRECTIONAL_REVERSE, BIDIRECTIONAL };
-
-// Warning: flow_t must be able to handle the sum of flows, not just individual edges.
-template<typename flow_t>
-struct dinic {
-    struct edge {
-        int node, rev;
-        flow_t capacity;
-        edge_type type;
-
-        edge() {}
-
-        edge(int _node, int _rev, flow_t _capacity, edge_type _type)
-            : node(_node), rev(_rev), capacity(_capacity), type(_type) {}
+        Edge() {}
+        Edge(int _v, int _rev, ll _cap, ll _ocap, e_type _type) : v(_v), rev(_rev), cap(_cap), ocap(_ocap), type(_type) {}
     };
 
-    int V = -1;
-    vector<vector<edge>> adj;
-    vector<int> dist;
-    vector<int> edge_index;
-    bool flow_called = false;
+    int n;
+    vector<vector<Edge>> g;
+    vi level, next;
+    vector<bool> vis;
 
-    dinic(int vertices = -1) {
-        if (vertices >= 0)
-            init(vertices);
+    void init(int _n){
+        n = _n;
+        g.resize(n, {});
     }
-
-    void init(int vertices) {
-        V = vertices;
-        adj.assign(V, {});
-        flow_called = false;
+    void add_edge(int u, int v, ll c1, ll c2, e_type t1, e_type t2){
+        Edge uv(v, si(g[v]) + (u == v), c1, c1, t1);
+        Edge vu(u, si(g[u]), c2, c2, t2);
+        g[u].pb(uv);
+        g[v].pb(vu);
     }
-
-    void _add_edge(int u, int v, flow_t capacity1, flow_t capacity2, edge_type type1, edge_type type2) {
-        assert(0 <= min(u, v) && max(u, v) < V);
-        assert(capacity1 >= 0 && capacity2 >= 0);
-        edge uv_edge(v, int(adj[v].size()) + (u == v ? 1 : 0), capacity1, type1);
-        edge vu_edge(u, int(adj[u].size()), capacity2, type2);
-        adj[u].push_back(uv_edge);
-        adj[v].push_back(vu_edge);
+    void add_directional_edge(int u, int v, ll c){
+        add_edge(u, v, c, 0, directional, directional_reverse);
     }
-
-    void add_directional_edge(int u, int v, flow_t capacity) {
-        _add_edge(u, v, capacity, 0, DIRECTIONAL, DIRECTIONAL_REVERSE);
+    void add_bidirectional_edge(int u, int v, ll c){
+        add_edge(v, u, c, c, bidirectional, bidirectional);
     }
-
-    void add_bidirectional_edge(int u, int v, flow_t capacity) {
-        _add_edge(u, v, capacity, capacity, BIDIRECTIONAL, BIDIRECTIONAL);
-    }
-
-    edge &reverse_edge(const edge &e) {
-        return adj[e.node][e.rev];
-    }
-
-    bool bfs(int source, int sink) {
-        vector<int> q(V);
-        int q_start = 0, q_end = 0;
-        dist.assign(V, INF);
-
-        auto bfs_check = [&](int node, int new_dist) -> void {
-            if (new_dist < dist[node]) {
-                dist[node] = new_dist;
-                q[q_end++] = node;
-            }
-        };
-
-        bfs_check(source, 0);
-
-        while (q_start < q_end) {
-            int top = q[q_start++];
-
-            for (edge &e : adj[top])
-                if (e.capacity > 0)
-                    bfs_check(e.node, dist[top] + 1);
-        }
-
-        return dist[sink] < INF;
-    }
-
-    flow_t dfs(int node, flow_t path_cap, int sink) {
-        if (node == sink)
-            return path_cap;
-
-        if (dist[node] >= dist[sink])
-            return 0;
-
-        flow_t total_flow = 0;
-
-        // Because we are only performing DFS in increasing order of dist, we don't have to revisit fully searched edges
-        // again later.
-        while (edge_index[node] < int(adj[node].size())) {
-            edge &e = adj[node][edge_index[node]];
-
-            if (e.capacity > 0 && dist[node] + 1 == dist[e.node]) {
-                flow_t path = dfs(e.node, min(path_cap, e.capacity), sink);
-                path_cap -= path;
-                e.capacity -= path;
-                reverse_edge(e).capacity += path;
-                total_flow += path;
-            }
-
-            // If path_cap is 0, we don't want to increment edge_index[node] as this edge may not be fully searched yet.
-            if (path_cap == 0)
-                break;
-
-            edge_index[node]++;
-        }
-
-        return total_flow;
-    }
-
-    // Can also be used to reverse flow or compute incremental flows after graph modification.
-    flow_t max_flow(int source, int sink, flow_t flow_cap = numeric_limits<flow_t>::max()) {
-        assert(V >= 0);
-        flow_called = true;
-        flow_t total_flow = 0;
-
-        while (flow_cap > 0 && bfs(source, sink)) {
-            edge_index.assign(V, 0);
-            flow_t increment = dfs(source, flow_cap, sink);
-            assert(increment > 0);
-            total_flow += increment;
-            flow_cap -= increment;
-        }
-
-        return total_flow;
-    }
-
-    vector<bool> reachable;
-
-    void _reachable_dfs(int node) {
-        reachable[node] = true;
-
-        for (edge &e : adj[node])
-            if (e.capacity > 0 && !reachable[e.node])
-                _reachable_dfs(e.node);
-    }
-
-    void solve_reachable(int source) {
-        reachable.assign(V, false);
-        _reachable_dfs(source);
-    }
-
-    // Returns a list of {capacity, {from_node, to_node}} representing edges in the min cut.
-    vector<pair<flow_t, pair<int, int>>> min_cut(int source) {
-        assert(flow_called);
-        solve_reachable(source);
-        vector<pair<flow_t, pair<int, int>>> cut;
-
-        for (int node = 0; node < V; node++)
-            for (edge &e : adj[node])
-                if (reachable[node] && !reachable[e.node] && e.type != DIRECTIONAL_REVERSE) {
-                    flow_t rev_cap = reverse_edge(e).capacity;
-                    flow_t original_cap = e.type == BIDIRECTIONAL ? rev_cap / 2 : rev_cap;
-                    cut.emplace_back(original_cap, make_pair(node, e.node));
+    int bfs(int s, int t){
+        level.assign(n, -1);
+        next.assign(n, 0);
+        level[s] = 0;
+        queue<int> q;
+        q.push(s);
+        while (si(q)){
+            int u = q.front(); q.pop();
+            for (Edge& e : g[u]){
+                if (level[e.v] == -1 && e.cap){
+                    level[e.v] = level[u] + 1;
+                    q.push(e.v);
                 }
-
-        return cut;
+            }
+        }
+        return level[t];
     }
-
-    // Helper function for setting up incremental / reverse flows. Can become invalid if adding additional edges.
-    edge *find_edge(int a, int b) {
-        for (edge &e : adj[a])
-            if (e.node == b)
-                return &e;
-
-        return nullptr;
+    ll dfs(int u, int t, ll cur){
+        if (u == t) return cur;
+        for (; next[u] != si(g[u]); ++ next[u]){
+            Edge& e = g[u][next[u]];
+            if (level[e.v] == level[u] + 1 && e.cap){
+                ll val = dfs(e.v, t, min(cur, e.cap));
+                if (val){
+                    e.cap -= val;
+                    g[e.v][e.rev].cap += val;
+                    return val;
+                }
+            }
+        }
+        return 0;
+    }
+    ll max_flow(int s, int t){
+        ll res = 0;
+        while (bfs(s, t) != -1){
+            while (ll cur = dfs(s, t, LLONG_MAX)){
+                res += cur;
+            }
+        }
+        return res;
+    }
+    void dfs2(int u){
+        vis[u] = true;
+        for (Edge& e : g[u]){
+            if (!vis[e.v] && e.cap){
+                dfs2(e.v);
+            }
+        }
+    }
+    vvl min_cut(int s, int t){
+        vis.resize(n, false);
+        dfs2(s);
+        vvl res;
+        fr(u, n){
+            if (!vis[u]) continue;
+            for (Edge& e : g[u]){
+                if (!vis[e.v] && e.type != directional_reverse){
+                    ll flow = g[e.v][e.rev].cap;
+                    if (e.type == bidirectional) flow /= 2;
+                    res.pb({u, e.v, flow});
+                }
+            }
+        }
+        return res;
     }
 };
-
