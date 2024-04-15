4a806344d4776588c808af9f429ddab5ed458816
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2024-04-15 16:27:06 +0530

    Update sos dp .cpp

--- templates/dp /subset dp/sos dp .cpp
+++ templates/dp /subset dp/sos dp .cpp
@@ -5,6 +5,21 @@ time complexity to find f(x) for all nubmers upto n : O(NLogN)
 
 max_log = ceil(log2(max_value)) + 1
 
+1) if we want count of elements that are subset/superset of x, then store count of each element in dp and use take_contribution from 
+    subset/supserset
+    
+2) if we want count of elements that are exactly x, then first do method 1, then use remove_contribution from 
+    subset/supserset
+    
+3) if we want count of subsets that are subset/superset of x, then first use method 1 to store count of elements
+    that are subset/supserset of x, then change dp[i] to (1 << dp[i]) - 1
+    
+4) if we want count of subsets that are exactly x, then first use method 3 to generate count of subsets that are
+    subset/superset of x, then use remove_contribution to get count of subsets/supersets that are exactly x
+    
+5) remove_contribution_from_subset/supserset are undo operations of take contributions from subset/superset
+
+
 */
 
 const ll max_log = 20;
