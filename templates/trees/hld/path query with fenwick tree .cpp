c7758695bd24d430d3054f11110f1e104aa1b30c
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2024-08-10 01:06:21 +0530

    Update path query with fenwick tree .cpp

--- templates/trees/hld/path query with fenwick tree .cpp
+++ templates/trees/hld/path query with fenwick tree .cpp
@@ -19,6 +19,8 @@ struct FenwickTree{
         }
     }
     void update(int l, int r, ll x){
+        l += 1;
+        r += 1;
         point_update(l, x, b1);
         point_update(r + 1, -x, b1);
         point_update(l, x * (l - 1), b2);
@@ -35,7 +37,10 @@ struct FenwickTree{
         return sum(idx, b1) * idx - sum(idx, b2);
     }
     ll query(int l, int r){
-        return prefix_sum(r) - prefix_sum(l - 1);
+        return prefix_sum(r + 1) - prefix_sum(l - 1 + 1);
+    }
+    void set_value(int idx, ll val){
+        update(idx, idx, val - query(idx, idx));
     }
 };
 
@@ -108,21 +113,21 @@ struct Tree{
         ll res = 0;
         while (chain[u] != chain[v]){
             if (level[chain[u]] < level[chain[v]]) swap(u, v);
-            res += tree.query(id[chain[u]] + 1, id[u] + 1);
+            res += tree.query(id[chain[u]], id[u];
             u = par[chain[u]];
         }
         if (level[u] > level[v]) swap(u, v);
-        return res + tree.query(id[u] + 1, id[v] + 1);
+        return res + tree.query(id[u], id[v]);
     }
 
     void update_path(int u, int v, ll x){
         while (chain[u] != chain[v]){
             if (level[chain[u]] < level[chain[v]]) swap(u, v);
-            tree.update(id[chain[u]] + 1, id[u] + 1, x);
+            tree.update(id[chain[u]], id[u], x);
             u = par[chain[u]];
         }
         if (level[u] > level[v]) swap(u, v);
-        tree.update(id[u] + 1, id[v] + 1, x);
+        tree.update(id[u], id[v], x);
     }
 
     void set_value_node(int u, ll x){
