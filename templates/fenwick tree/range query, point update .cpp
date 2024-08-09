08cdb6cad05b1668dca01e7b09876c4a417ead5d
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2024-08-10 00:51:34 +0530

    Update range query, point update .cpp

--- templates/fenwick tree/range query, point update .cpp
+++ templates/fenwick tree/range query, point update .cpp
@@ -7,17 +7,17 @@ struct FenwickTree{
         b.resize(n + 1, 0);
     }
     void update(int idx, ll x){
-        for (int i = idx; i <= n; i += i & -i) b[i] += x;
+        for (int i = idx + 1; i <= n; i += i & -i) b[i] += x;
     }
     void set_value(int idx, ll x){
-        update(idx, x - query(idx, idx));
+        update(idx + 1, x - query(idx + 1, idx + 1));
     }
     ll sum(int idx){
         ll res = 0;
-        for (int i = idx; i > 0; i -= i & -i) res += b[i];
+        for (int i = idx + 1; i > 0; i -= i & -i) res += b[i];
         return res;
     }
     ll query(int l, int r){
-        return sum(r) - sum(l - 1);
+        return sum(r + 1) - sum(l - 1 + 1);
     }
 };
