85d35b4d56ac6f17b5e53248bc7fbe1cc2cef650
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2024-08-13 15:02:45 +0530

    Update base_template.cpp

--- templates/base_template.cpp
+++ templates/base_template.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 const int mod = 1000000007;
 const int mod2 = 998244353;
+mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+
 #define ll long long
 #define all(a) a.begin(), a.end()
 #define pb push_back
