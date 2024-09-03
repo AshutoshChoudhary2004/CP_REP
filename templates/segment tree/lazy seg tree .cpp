efef89a28b89e947ce64b8f5f26b44601cd9528e
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2024-09-03 22:37:32 +0530

    Update lazy seg tree .cpp

--- templates/segment tree/lazy seg tree .cpp
+++ templates/segment tree/lazy seg tree .cpp
@@ -3,7 +3,7 @@ Example for range min
 whatever is the deafult value, is the default value in seg tree
 if dont want that than use set_value to set value
 */
-template <typename T, typename U, T default_value, U lazy_default_value, T (*combine)(T, T), U (*combine_lazy)(U, U), T (*add_lazy)(T, U, int, int)>
+template <typename T, typename U, T (*default_value)(), U (*lazy_default_value)(), T (*combine)(T, T), U (*combine_lazy)(U, U), T (*add_lazy)(T, U, int, int)>
 struct SegmentTree{
     int n;
     vector<T> seg;
@@ -11,13 +11,13 @@ struct SegmentTree{
 
     void init(int _n){
         n = _n;
-        seg.resize(4 * n, default_value); 
-        lazy.resize(4 * n, lazy_default_value);
+        seg.resize(4 * n, default_value()); 
+        lazy.resize(4 * n, lazy_default_value());
     }
     T query(int ind, int low, int high, int left, int right){
-        set_lazy(ind, low, high, lazy_default_value);
+        set_lazy(ind, low, high, lazy_default_value());
         if (low >= left && high <= right) return seg[ind];
-        if (high < left || low > right) return default_value;
+        if (high < left || low > right) return default_value();
         int mid = (low + high) >> 1;
         return combine(query(2 * ind + 1, low, mid, left, right), query(2 * ind + 2, mid + 1, high, left, right));
     }
@@ -28,10 +28,10 @@ struct SegmentTree{
             lazy[2 * ind + 2] = combine_lazy(lazy[ind], lazy[2 * ind + 2]);
         }
         seg[ind] = add_lazy(seg[ind], lazy[ind], low, high);
-        lazy[ind] = lazy_default_value;
+        lazy[ind] = lazy_default_value();
     }
     void set_value(int ind, int low, int high, int idx, T x){
-        set_lazy(ind, low, high, lazy_default_value);
+        set_lazy(ind, low, high, lazy_default_value());
         if (low == high){
             seg[ind] = x;
             return;
@@ -47,7 +47,7 @@ struct SegmentTree{
         set_value(0, 0, n - 1, i, val);
     }
     void update(int ind, int low, int high, int left, int right, U val){
-        set_lazy(ind, low, high, lazy_default_value);
+        set_lazy(ind, low, high, lazy_default_value());
         if (low >= left && high <= right){
             return set_lazy(ind, low, high, val);
         }
@@ -66,6 +66,8 @@ struct SegmentTree{
     }
 };
 
+ll default_value(){return (ll)1e18;}
+ll lazy_default_value(){return 0LL;}
 ll combine(ll x, ll y){return min(x,  y);}
 ll combine_lazy(ll x, ll y){return x + y;}
 ll add_lazy(ll x, ll y, int low, int high){return x + y;}
@@ -74,10 +76,11 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    SegmentTree<ll, ll, (ll)(1e18), 0LL, combine, combine_lazy, add_lazy> tree;
-   
+    SegmentTree<ll, ll, default_value, lazy_default_value, combine, combine_lazy, add_lazy> tree;
+    
     int n;
     cin >> n;
+
     tree.init(n);
 
     for (int i = 0; i < n; ++ i){
@@ -85,5 +88,4 @@ int main(){
         cin >> a;
         tree.set_value(i, a);
     } 
-
 }
