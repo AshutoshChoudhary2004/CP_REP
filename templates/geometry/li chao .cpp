258fbb8fa033fbd3a8f406572148c6a7513fd392
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2024-10-06 15:48:39 +0530

    Update li chao .cpp

--- templates/geometry/li chao .cpp
+++ templates/geometry/li chao .cpp
@@ -1,4 +1,7 @@
 /* 
+
+TC : log2(max_x - min_x)
+
 f(x) = mx + c
 Gives the max value of f(x)
 
@@ -12,11 +15,24 @@ max value = query (x)
 to add line : add_line(-m, -c)
 min_value = - query(-x)
 
+def_val : The default value of x
+
 To initialize pass whatever is the min_x and max_x that you will query
-Dont pass too big / small values of min_x and max_x otherwise you will get overflow
-if we are getting wrong answer or we want to pass large min_x and max_x then use __int28
+
+if ans <= 1e18 or f(x) <= 1e18 then use ll, and defVal = -1e18
+if ans > 1e18 or f(x) > 1e18 then use __int128 and defVal = -1e36
+
 
 def_val : The default value of x, pass -1e18 or -1e9
+
+min_x : the min value of x that you will query
+max_x : the max value of x that you will query
+
+we dont need exact values of min_x and max_x upper bound is fine
+for eg if min_x is -1e3 you can pass -1e5
+and if max_x is around 1e5 you can pass 1e6
+
+TC will be same, but don't pass veryyy big values, otherwise it might overflow
 
 */
 template<typename T, T min_x, T max_x, T def_val>
