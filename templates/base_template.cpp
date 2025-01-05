c55aa45f514d776609aba412d91279df8a8097d1
AshutoshChoudhary2004 <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2025-01-05 15:22:42 +0530

    Update base_template.cpp

--- templates/base_template.cpp
+++ templates/base_template.cpp
@@ -5,7 +5,9 @@ constexpr int mod = 1000000007;
 constexpr int mod2 = 998244353;
 mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 
-#define ll long long
+typedef long long ll;
+typedef long double ld;
+
 #define all(a) a.begin(), a.end()
 #define pb push_back
 #define ins insert
@@ -24,6 +26,9 @@ mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 #define max5(a, b, c, d, e) max2(max4(a, b, c, d), e)
 #define max6(a, b, c, d, e, f) max2(max4(a, b, c, d), max2(e, f))
 #define max7(a, b, c, d, e, f, g) max2(max4(a, b, c, d), max3(e, f, g))
+#define max8(a, b, c, d, e, f, g, h) max2(max4(a, b, c, d), max4(e, f, g, h))
+#define max9(a, b, c, d, e, f, g, h, i) max2(max8(a, b, c, d, e, f, g, h), i)
+#define max10(a, b, c, d, e, f, g, h, i, j) max2(max8(a, b, c, d, e, f, g, h), max2(i, j))
 
 #define min2(a, b) ((a) < (b) ? (a) : (b))
 #define min3(a, b, c) min2(min2(a, b), c)
@@ -31,25 +36,40 @@ mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 #define min5(a, b, c, d, e) min2(min4(a, b, c, d), e)
 #define min6(a, b, c, d, e, f) min2(min4(a, b, c, d), min2(e, f))
 #define min7(a, b, c, d, e, f, g) min2(min4(a, b, c, d), min3(e, f, g))
+#define min8(a, b, c, d, e, f, g, h) min2(min4(a, b, c, d), min4(e, f, g, h))
+#define min9(a, b, c, d, e, f, g, h, i) min2(min8(a, b, c, d, e, f, g, h), i)
+#define min10(a, b, c, d, e, f, g, h, i, j) min2(min8(a, b, c, d, e, f, g, h), min2(i, j))
+
 
 typedef vector<int> vi;
-typedef vector<ll> vl;
-
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<int, ll> pil;
-typedef pair<ll, int> pli;
-
 typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-typedef vector<pli> vpli;
-typedef vector<pil> vpil;
-
 typedef vector<vvi> vvvi;
 typedef vector<vvvi> vvvvi;
 typedef vector<vvvvi> vvvvvi;
+typedef vector<vvvvvi> vvvvvvi;
+
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef vector<vvpii> vvvpii;
+typedef vector<vvvpii> vvvvpii;
+typedef vector<vvvvpii> vvvvvpii;
+typedef vector<vvvvvpii> vvvvvvpii;
+
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vvl> vvvl;
+typedef vector<vvvl> vvvvl;
+typedef vector<vvvvl> vvvvvl;
+typedef vector<vvvvvl> vvvvvvl;
+
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+typedef vector<vpll> vvpll;
+typedef vector<vvpll> vvvpll;
+typedef vector<vvvpll> vvvvpll;
+typedef vector<vvvvpll> vvvvvpll;
+typedef vector<vvvvvpll> vvvvvvpll;
 
 template <typename T>
 void get_unique(vector<T>& a) {
@@ -68,8 +88,20 @@ template <class T> ostream& operator<<(ostream& os, const multiset<T>& p) { os <
 template <class S, class T> ostream& operator<<(ostream& os, const map<S, T>& p) { os << "[ "; for (auto it = p.begin(); it != p.end(); ++it) { os << "( " << it->first << ": " << it->second << " )" << (it != prev(p.end()) ? ", " : ""); } os << " ]"; return os; }
 template <class T> ostream& operator<<(ostream& os, const unordered_multiset<T>& p) { os << "[ "; for (auto it = p.begin(); it != p.end(); ++it) { os << *it; if (next(it) != p.end()) { os << ", "; } } os << " ]"; return os; }
 
+void ashuhua(){
+    int n;
+    cin >> n;
+    vi a(n);
+    fr(i, n) cin >> a[i];
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int t = 1;
+    cin >> t;
+    while (t --){
+        ashuhua();
+    }
 }
