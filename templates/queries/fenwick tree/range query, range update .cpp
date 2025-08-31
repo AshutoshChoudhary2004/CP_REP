0c6aaa6dff1ec413b29894874795c9567caab335
Ashutosh Choudhary <132291166+AshutoshChoudhary2004@users.noreply.github.com>
2025-08-31 18:46:34 +0530

    Add files via upload

---
+++ templates/queries/fenwick tree/range query, range update .cpp
@@ -0,0 +1,37 @@
+struct FenwickTree{
+    int n;
+    vl b1, b2;
+
+    void init(int _n){
+        n = _n;
+        b1.resize(n + 1, 0);
+        b2.resize(n + 1, 0);
+    }
+    void point_update(int idx, ll x, vl &b){
+        for (int i = idx + 1; i <= n; i += i & -i){
+            b[i] += x;
+        }
+    }
+    void update(int l, int r, ll x){
+        point_update(l, x, b1);
+        point_update(r + 1, -x, b1);
+        point_update(l, x * (l - 1), b2);
+        point_update(r + 1, -x * r, b2);
+    }
+    ll sum(int idx, vl &b){
+        ll res = 0;
+        for (int i = idx + 1; i > 0; i -= i & -i){
+            res += b[i];
+        } 
+        return res;
+    }
+    ll prefix_sum(int idx){
+        return sum(idx, b1) * idx - sum(idx, b2);
+    }
+    ll query(int l, int r){
+        return prefix_sum(r) - prefix_sum(l - 1);
+    }
+    void set_value(int idx, ll val){
+        update(idx, idx, val - query(idx, idx));
+    }
+};
